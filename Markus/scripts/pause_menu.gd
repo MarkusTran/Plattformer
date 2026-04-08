c234a7332c5d6b6ea756332f4080380544599db1
MarkusTran <m.tran1@students.htl-leonding.ac.at>
2026-04-08 12:00:43 +0200

    verschone mich mit dem Dreck

--- Markus/scripts/pause_menu.gd
+++ Markus/scripts/pause_menu.gd
@@ -1,36 +1,25 @@
 extends Control
 
-
 var game_paused : bool = false
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	hide()
 
-
 func _input(event: InputEvent) -> void:
-	if (event.is_action_pressed("ui_cancel")):
-		if (game_paused):
-			game_paused = false
-		else :
-			game_paused = true
-			
-	_on_toggle_game_paused(game_paused)
-		
+	if event.is_action_pressed("ui_cancel"):
+		game_paused = !game_paused
+		_on_toggle_game_paused(game_paused)
 
-
-func _on_toggle_game_paused(is_paused : bool):
-	if(is_paused):
-		get_tree().paused = true
+func _on_toggle_game_paused(is_paused: bool) -> void:
+	get_tree().paused = is_paused
+	if is_paused:
 		show()
 	else:
-		get_tree().paused = false
 		hide()
 
-
 func _on_resume_pressed() -> void:
-	get_tree().paused = false
-	hide()
-
+	game_paused = false
+	_on_toggle_game_paused(false)
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
